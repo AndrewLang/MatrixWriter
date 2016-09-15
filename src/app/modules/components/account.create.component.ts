fa5dea74a5689d614de3d7304a56e1cf241789a1
Andy <nnlyx@hotmail.com>
2016-09-14 23:39:23 -0600

    create account layout

--- src/app/modules/components/account.create.component.ts
+++ src/app/modules/components/account.create.component.ts
@@ -2,31 +2,43 @@ import {Component, OnInit, OnDestroy}        from '@angular/core';
 import { Router }                            from '@angular/router';
 
 @Component({
-    templateUrl:'src/views/account.create.html'
+    templateUrl: 'src/views/account.create.html'
 })
-export class CreateAccountComponent{
-    Title:string;
-    CurrentStep: string;
+export class CreateAccountComponent {
+    Title: string;
+    CurrentStep: number = 1;
+    TotalSteps: number = 4;
 
+    constructor(private mRouter: Router) {
+        this.Title = "Add a blog account";
+    }
     Next(): void {
+        if (this.CurrentStep < this.TotalSteps)
+            this.CurrentStep++;
 
     }
-    CanGoNext():boolean{
-        return true;
+    CanGoNext(): boolean {
+        return this.CurrentStep < this.TotalSteps;
     }
-    Previous():void{
-
+    Previous(): void {
+        if (this.CurrentStep > 1)
+            this.CurrentStep--;
     }
-    CanGoPrevious(): boolean{
-        return true;
+    CanGoPrevious(): boolean {
+        return this.CurrentStep > 1;
     }
-    Finish():void{
-
+    Finish(): void {
+        this.mRouter.navigate(['welcome']);
     }
-    
+    CanFinish(): boolean {
+        return this.CurrentStep == this.TotalSteps;
+    }
+    Cancel(): void {
+        this.mRouter.navigate(['welcome']);
+    }
 }
 
-export class ActionStep{
+export class ActionStep {
     Name: string;
     IsActive: boolean = false;
 }
