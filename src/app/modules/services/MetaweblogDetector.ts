11f5e8ea0140fb05855ee2d139353d533a1181f9
AndrewLang <nnlyx@hotmail.com>
2016-09-15 16:48:55 -0600

    Call detector

--- src/app/modules/services/MetaweblogDetector.ts
+++ src/app/modules/services/MetaweblogDetector.ts
@@ -12,25 +12,29 @@ import {DataService}    from './DataService';
 @Injectable()
 export class MetaweblogDetector implements IBlogDetector {
 
-    constructor(private dataService: DataService) {}
+
+    constructor(private dataService: DataService) { }
 
     Detect(account: BlogAccount): Promise<Array<Blog>> {
-        if( !this.dataService)
+        if (!this.dataService)
             throw new Error("Data server is not found.");
 
+        let self = this;
         return new Promise(function (resolve, reject) {
             let blogs: Array<Blog> = [];
 
-            this.dataService.Get(account.HomeUrl, (response: any) => {
+            self.dataService.Get(account.HomeUrl, (response: any) => {
                 var parser = new DOMParser()
                 var doc = parser.parseFromString(response._body, "text/html");
 
                 let rsdLink = doc.querySelector("link[title=RSD]").getAttribute("href");
                 console.log("RSD Link: " + rsdLink);
 
-                this.dataService.Get(rsdLink, (rsdResponse: any) => {
+                self.dataService.Get(rsdLink, (rsdResponse: any) => {
                     let xml = parser.parseFromString(rsdResponse._body, "text/xml");
                     let apiLink = xml.querySelector("service apis api").getAttribute("apiLink");
+
+                    console.log("Api link " + apiLink);
 
                     var blog = new Blog();
                     blog.HomeUrl = account.HomeUrl;
